32d88b2d5b638fe373d397ddf552e06c51896865
Aryan <aryan.kapur@cogentinfo.com>
2024-07-15 12:11:01 -0400

    Update DETECTION_SIMULATOR.py

--- pages/DETECTION_SIMULATOR.py
+++ pages/DETECTION_SIMULATOR.py
@@ -6,7 +6,7 @@ from PIL import Image
 import os
 
 # Ensure the parent directory is in the sys.path
-from ..utils import load_model, get_predictions, draw_boxes
+from parking_slot_detection.utils import load_model, get_predictions, draw_boxes
 
 def appp():
 
